827f5f2dd11eb8d9a4ca5cea7d12ef11e0a5f6e1
Jéssica Gouveia <jehsgouveia@gmail.com>
2021-05-20 23:40:56 -0300

    Adicionando exercícios aula 2

---
+++ exercicio2.js
@@ -0,0 +1,16 @@
+// 2 - Calcule o tempo de uma viagem de carro. Pergunte a distância a percorrer e a velocidade média esperada para a viagem.
+
+let velocidadeMedia = 80
+
+let distanciaEmKm = 240
+
+let tempoHoras = distanciaEmKm / velocidadeMedia
+
+console.log(`O tempo da viagem será de ${tempoHoras} horas`)
+
+
+
+
+
+
+
---
+++ exercicio5.js
@@ -0,0 +1,14 @@
+// 5 - Receba dos usuários 3 números e calcule a média entre eles.
+
+let num1 = 20
+
+let num2 = 40
+
+let num3 = 60
+
+let resultadoSoma = num1 + num2 + num3
+
+let media = resultadoSoma / 3
+
+console.log(media)
+
